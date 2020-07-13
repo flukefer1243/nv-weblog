7f1e949ab54823c0eb2112a4d2904c6954fc1169
nattaponkum <nattaponk@nu.ac.th>
2020-07-13 14:10:18 +0700

    latest code from Chapter6page 84

--- server/src/app.js
+++ server/src/app.js
@@ -1,10 +1,13 @@
 let express = require('express')
 const app = express()
 let bodyParser = require('body-parser')
+const {sequelize} = require('./models')
+const config = require('./config/config')
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: true}))
 
+require('./routes')(app)
 
 app.get('/status', function (req, res){
     res.send('Hello nodejs server')
@@ -24,7 +27,6 @@ app.get('/users', function (req, res) {
     res.send('เรียกข้อมูลผู้ใช้งานทั้งหมด')
 })
 
-let port = 8081
 
 app.listen(port, function () {
     console.log(('server running on '+ port))
@@ -43,4 +45,11 @@ app.put('/user/:userId', function (req, res) {
 app.delete('/user/:userId', function (req, res) {
     res.send('ทำการลบผู้ใช้งาน: ' + req.params.userId + ' : ' +
     JSON.stringify(req.body))
+})
+
+let port = process.env.PORT || config.port
+sequelize.sync({force: false}).then(() => {
+    app.listen(port, function () {
+        console.log('Server running on ' + port)
+    })
 })
